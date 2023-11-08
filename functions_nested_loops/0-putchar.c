636708b7ef601dec6c9e19678574a48343bd75bf
suareza1992 <suareza1992@gmail.com>
2023-11-08 16:08:17 -0400

    print alphabet

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,9 @@
-#include <unistd.h>
-#include "holberton.h"
 #include <stdio.h>
+
 /**
- * _putchar - prints the character c to stdout
- * @c: The character to be printed
- *
- * Return: On Success 1.
- * On error,- 1 is returned , and errno is set.
- */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
-}
+	printf("_putchar\n");
+	return (0);
+
