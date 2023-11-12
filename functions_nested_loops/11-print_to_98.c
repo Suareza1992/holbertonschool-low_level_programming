b67a314c22a4bbf2cc0dd6e5933f46f47a2f37d3
suareza1992 <suareza1992@gmail.com>
2023-11-12 01:48:46 -0400

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-	for (; n >= 98; n++)
+	for (; n <= 98; n++)
 	{
 	if (n == 98)
 	{
@@ -30,7 +30,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n >= 98; n--)
+	for (; n >= 98; n--)
 	{
 	if (n == 98)
 	{
@@ -39,7 +39,8 @@ void print_to_98(int n)
 	break;
 	}
 	else
-	{printf("%d, ", n);
+	{
+	printf("%d, ", n);
 	}
 	}
 	}
