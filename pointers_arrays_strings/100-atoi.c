8cea355caf9d8130d2d9abfe7a78fbed0d791409
Angel Suarez <angelsuarez@Angels-MBP.localdomain>
2024-02-27 13:45:28 -0400

    100 atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
-#include "main.c"
-#include <stdio.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
 * _atoi - This function converts a string to an integer.
@@ -15,16 +15,23 @@ int _atoi(char *s)
 	i = num = 0;
 	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-		if (num >= 0)
-		{
-			num = num * 10 - (s[i] - '0');
-			i++;
-		}
-		else
-		{
-			num = num * 10 - (s[i] - '0');
-			i++;
-		}
+		if (s[i] == '-')
+			negative = negative * -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		num = num * 10 - (s[i] - '0');
+		i++;
+	}
+	else
+	{
+		num = num * 10 - (s[i] - '0');
+		i++;
+	}
+	negative = negative * -1;
+	return (num * negative);
+}
 	}
 	negative = negative * -1;
 	return (num * negative);
