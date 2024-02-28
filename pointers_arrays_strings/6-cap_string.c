3a1bb9feab48eb76fbb99267b97dd48a881237cc
Angel Suarez <angelsuarez@Angels-MBP.localdomain>
2024-02-28 14:21:26 -0400

    pointers 2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: Pointer to the input string
+ *
+ * Return: Pointer to the string changed.
+ */
+char *cap_string(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] >= 'a' && s[len] <= 'z' && len == 0)
+			s[len] -= ' ';
+		switch (s[len - 1])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case 63:
+			case '!':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (s[len] >= 'a' && s[len] <= 'z')
+					s[len] -= ' ';
+				break;
+		}
+		len++;
+	}
+	return (s);
+}
