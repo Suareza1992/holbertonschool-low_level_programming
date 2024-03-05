d77a2da149e466255e75c5e6b2a2fc73c605ed6f
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-03-05 13:30:03 -0400

    my new name final

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,8 +4,9 @@
 /**
  * main - Entry point
  *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
+ * @argc: Argument counter. Counts the number of parameters that go into main
+ * @argv: Argument vector. Pointer of array of pointers containing strings 
+ * entering main
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
